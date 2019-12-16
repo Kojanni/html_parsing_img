bd959578c917687d0c401cf6c498b18f8f993d64
KEA <kjanni22@gmail.com>
2019-12-16 17:45:25 +0300

    Changed Main, added class ImageStorage and ParseImage

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,15 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.*;
-import java.net.URL;
+import java.util.List;
 
 public class Main {
     private static String SITE_URL = "http://lenta.ru/";
     private static String SEPARATOR = File.separator;
     private static String IMAGE_FOLDER = "C:" + SEPARATOR + "Users" + SEPARATOR + "ASUS" + SEPARATOR + "Pictures";
 
-
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect(SITE_URL).get();
-
-        Elements element = doc.select("img");
-        for (Element e : element) {
-            String imgURL = e.attr("abs:src");
-            downloadImg(imgURL);
-        }
-    }
-
-    private static void downloadImg(String imgURL) throws IOException {
-        String imgName = imgURL.substring(imgURL.lastIndexOf("/") + 1);
-        if ((imgName.contains(".jpg")) || (imgName.contains(".bmp")) || (imgName.contains(".tif"))
-                || (imgName.contains(".png")) || (imgName.contains(".jpeg")) || (imgName.contains(".gif")) || (imgName.contains(".tiff"))) {
-            System.out.println("Save image\nURL: " + imgURL + "\nImage name: " + imgName);
-            try {
-                URL urlImg = new URL(imgURL);
-                InputStream in = urlImg.openStream();
-                byte[] buffer = new byte[5000];
-                int n = -1;
-                OutputStream out = new FileOutputStream(IMAGE_FOLDER + SEPARATOR + imgName);
-                while ((n = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, n);
-                }
-                out.close();
-                System.out.println("IMAGE SAVED");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        ParseImage parseImage = new ParseImage();
+        List<String> imageUrls = parseImage.parse(SITE_URL);
+        ImageStorage imageStorage = new ImageStorage(imageUrls);
+        imageStorage.download(IMAGE_FOLDER);
     }
 }
